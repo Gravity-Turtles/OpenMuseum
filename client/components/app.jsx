e294d6b863a1d9789044bfc8f51a84cdb7570dbc
Matthew Yun <myun14@gmail.com>
2016-09-17 15:03:48 -0400

    bug fix

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,11 +1,12 @@
-import { React, Component } from 'react';
+import React from 'react';
+import { Component } from 'react';
 
 export default class App extends Component {
   render() {
     return (
       <main>
         <h1>
-            Open Museum!
+            Open Museum!!
         </h1>
       </main>
     );
