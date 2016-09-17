d9af104fedb563e7bdc4a30635b642009543996b
Matthew Yun <myun14@gmail.com>
2016-09-17 14:18:10 -0400

    ESLint setup

---
+++ client/routes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route, IndexRoute, Router } from 'react-router';
+
+import App from './components/app';
+
+export default (
+  <Route path="/" component={App} />
+);
+
