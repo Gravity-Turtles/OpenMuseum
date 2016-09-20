56253725e2c232919f3feee3ce60e2de6b30d85a
Matthew Yun <myun14@gmail.com>
2016-09-20 11:33:38 -0400

    artController mod

--- server/controllers/artController.js
+++ server/controllers/artController.js
@@ -9,27 +9,25 @@ module.exports.profileRead = function(req, res) {
 //********* Every secured page will have to go through this check **********//
 
 //**** need to add some error handling for if payloadId provided but not found
-  if (!req.payload._id) {
-    res.status(401).json({
-      "message" : "UnauthorizedError: private profile"
-    });
-  } else {
-    User
-      .findById(req.payload._id)
-      .exec(function(err, user) {
-        res.status(200).json(user);
-      });
-  }
+//   if (!req.payload._id) {
+//     res.status(401).json({
+//       "message" : "UnauthorizedError: private profile"
+//     });
+//   } else {
+//     User
+//       .findById(req.payload._id)
+//       .exec(function(err, user) {
+//         res.status(200).json(user);
+//       });
+//   }
 
-};
+// };
 
 //****** Insert New Art *******//
 
-module.exports.insertArt = function(req, res) {
-  console.log('insertArt running')  
-  console.log(req)
+module.exports.insertArt = function(req, res) {  
+  console.log('insertArt running')
   console.log(req.body)
-  // console.log(req.data)
     var art = new Art();
 
     art.title = req.body.title;    
