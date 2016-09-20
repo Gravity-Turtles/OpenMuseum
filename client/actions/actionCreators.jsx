944e158dc27c4b0f47be7f0b8fcbc1a043054bbd
DOH1211 <dohun.p@gmail.com>
2016-09-20 10:44:38 -0400

    Client: working on getLocation() and having asynch issue

--- client/actions/actionCreators.jsx
+++ client/actions/actionCreators.jsx
@@ -1,6 +1,6 @@
 // get Location
 
-export function getLocation(index) {
+export function getLocation() {
   const location = {};
   navigator.geolocation.getCurrentPosition(success, error);
   function success(position) {
@@ -17,3 +17,29 @@ export function getLocation(index) {
     location
   }
 }
+
+
+// export function getLocation() {
+//   let gotLoc = false;
+//   const location = {};
+//   navigator.geolocation.getCurrentPosition(success, error);
+//   function success(position) {
+//     location.latitude  = position.coords.latitude;
+//     location.longitude = position.coords.longitude;
+//     console.log(location.latitude, location.longitude);
+//     gotLoc = true;
+//   };
+
+//   function error() {
+//     console.log("Unable to retrieve your location");
+//   };
+
+//   while(true) {
+//     if(gotLoc) {
+//       return {
+//         type: 'GET_LOCATION',
+//         location: location
+//       }
+//     }
+//   }
+// }
