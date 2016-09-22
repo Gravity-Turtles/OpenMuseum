56245c95fb6be6bf7b4ec03a539de3dbaff19f8f
DOH1211 <dohun.p@gmail.com>
2016-09-22 16:50:53 -0400

    Client / feature: Google Maps

--- client/components/postDetail.jsx
+++ client/components/postDetail.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import GoogleMap from './googleMap';
 
 export default class PostDetail extends Component {
   render() {
@@ -6,10 +7,14 @@ export default class PostDetail extends Component {
     console.log(i);
     return (
       <main>
+        <div style={{width:'100%', height:'300px'}}>
+          <div style={{height:'100%'}}>
+            <GoogleMap lat={this.props.posts[i].locLat} lng={this.props.posts[i].locLong}/>
+          </div>
+        </div>
         <h1>{this.props.posts[i].title}</h1>
         <div>Images here</div>
         <div>{this.props.posts[i].description}</div>
-        <div>{this.props.posts[i].locLat} {this.props.posts[i].locLong}</div>
 
       </main>
     );
