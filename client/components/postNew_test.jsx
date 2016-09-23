b3ac108a105038d26fd6c750fb5be2b850d41471
mcqnyc <mcqnyc@gmail.com>
2016-09-23 17:51:11 -0400

    fix(Fixed indexing bug when posting new pictures): Fix indexing bug when posting new pictures
    Browser/React error in case multiple images uploaded are not indexed
    
    None

--- client/components/postNew_test.jsx
+++ client/components/postNew_test.jsx
@@ -8,9 +8,9 @@ class PostNew extends Component {
     super(props);
     this.state = {
       location: {
-      	latitude: 40.746727,
-      	longitude:	-73.987885
-      },	
+        latitude: 40.746727,
+        longitude:  -73.987885
+      },  
       title: '',
       description: '',
       files: [],
@@ -26,37 +26,37 @@ class PostNew extends Component {
   }
 
   onArtChange(event){
-  	this.setState({title: event.target.value});
+    this.setState({title: event.target.value});
   }
   
   onDescriptionChange(event){
-  	this.setState({description: event.target.value})
+    this.setState({description: event.target.value})
   }
 
   onImageChange(event){
-  	this.setState({image: event.target.value})
+    this.setState({image: event.target.value})
   }
 
   onDrop(file){
     console.log('Received DropZone files: ', file);
     var slicedArray = this.state.files.slice()
-	console.log("slicedArray", slicedArray)
-	slicedArray.push(file[0])
-	console.log(JSON.stringify(file[0]));
-	this.setState({ files: slicedArray })
-	console.log("after drop, current state.files: ", this.state.files, 'after push slicedArray', slicedArray);
+  console.log("slicedArray", slicedArray)
+  slicedArray.push(file[0])
+  
+  this.setState({ files: slicedArray })
+  console.log("after drop, current state.files: ", this.state.files, 'after push slicedArray', slicedArray);
     }
 
   onFormSubmit(event){
-  	
-  	event.preventDefault();
-  	let payload = this.state;
-  	console.log("in onFormSubmit!!! with state: ", this.state, "and payload: ", payload);
-  	
-  	axios.post('/api/art', payload)
+    
+    event.preventDefault();
+    let payload = this.state;
+    console.log("in onFormSubmit!!! with state: ", this.state, "and payload: ", payload);
+    
+    axios.post('/api/art', payload)
     .then(function(response){
     console.log('saved successfully')
-  });  	
+  });   
 
 
   }
@@ -66,19 +66,19 @@ class PostNew extends Component {
       <main>
         <h1>Post Page</h1>
         <br></br>
-        <form onSubmit={this.onFormSubmit} enctype="multipart/form-data">
+        <form onSubmit={this.onFormSubmit}>
           <input type="text" placeholder="Title of Artwork" value={this.state.title} onChange={this.onArtChange}/>
             <br></br>
-        	<textarea type="text" placeholder="Description" value={this.state.description} onChange={this.onDescriptionChange}/>
-        	<br></br> 
-        	 <Dropzone value={this.state.files} onDrop={this.onDrop}>
+          <textarea type="text" placeholder="Description" value={this.state.description} onChange={this.onDescriptionChange}/>
+          <br></br> 
+           <Dropzone value={this.state.files} onDrop={this.onDrop}>
               <div>Try dropping some files here, or click to select files to upload.</div>
             </Dropzone>
             <input type="submit"/>
             <br></br>
             {this.state.files.length > 0 ? <div>
                 <h2>Uploading {this.state.files.length} files...</h2>
-                <div>{this.state.files.map((file, i) => <img key={i} src={file.preview} /> )}</div>
+                <div>{this.state.files.map((file) => <img src={file.preview} /> )}</div>
                 </div> : null}
 
         </form>
